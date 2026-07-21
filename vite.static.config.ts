88ff9ed01e6ca48391700413178461c075ed6933
Saanchi Agarwal <saanchiagarwal@Saanchis-MacBook-Air.local>
2026-07-21 18:16:20 -0400

    Include preview assets in static build

--- vite.static.config.ts
+++ vite.static.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/detroit-connect-preview/",
   plugins: [react()],
-  publicDir: "public",
+  publicDir: "../public",
   root: "static",
   build: {
     emptyOutDir: true,
